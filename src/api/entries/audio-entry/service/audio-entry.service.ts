7dfae341da51e0931ddebad84b42b41ccb0023e5
tiberiuradu20 <tibibaesu@gmail.com>
2025-01-02 12:18:02 +0200

    Back-end Complet Modificat

--- src/api/entries/audio-entry/service/audio-entry.service.ts
+++ src/api/entries/audio-entry/service/audio-entry.service.ts
@@ -2,11 +2,46 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../../tenants/service/tenant.service';
 import { UpdateAudioEntryDto } from '../dto/update-audio-entry.dto';
 import { CreateAudioEntryDto } from '../dto/create-audio-entry.dto';
+import { subDays } from 'date-fns';
 @Injectable()
 export class AudioEntryService {
   constructor(private prisma: PrismaService) {}
 
+  async canAddAudioEntry(userId: number): Promise<boolean> {
+    // Găsește abonamentul utilizatorului
+    const subscription = await this.prisma.userSubscription.findFirst({
+      where: { userId },
+      include: { membership: true },
+    });
+  
+    if (!subscription) {
+      throw new Error('Utilizatorul nu are un abonament activ.');
+    }
+  
+    // Numără intrările scrise create azi
+    const todayStart = new Date();
+    todayStart.setHours(0, 0, 0, 0); // Începutul zilei curente
+  
+    const count = await this.prisma.audioEntry.count({
+      where: {
+        userId,
+        createdAt: {
+          gte: todayStart, // Filtrare pentru intrările create după începutul zilei
+        },
+      },
+    });
+  
+    // Compară cu limita zilnică
+    return count < subscription.membership.nr_maxim_intrari_Audio;
+  }
+  
+  
   async create(userId: number, fileUrl: string, duration: number) {
+     const adauga = await this.canAddAudioEntry(userId);
+     if(!adauga){
+      throw new Error("S-a atins limita maxima de intrari audio specifica abonamentului")
+      
+     }else{
     return this.prisma.audioEntry.create({
       data: {
         userId,
@@ -14,6 +49,7 @@ export class AudioEntryService {
         duration,
       },
     });
+  }
   }
 
   async findAllByUser(userId: number) {
--- src/api/entries/written-entry/service/written-entry.service.ts
+++ src/api/entries/written-entry/service/written-entry.service.ts
@@ -1,15 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../../../tenants/service/tenant.service';
 
-
 import { UpdateWrittenEntryDto } from '../dto/update-written-entry.dto';
-
+import { subDays } from 'date-fns';
 @Injectable()
 export class WrittenEntryService {
   constructor(private prisma: PrismaService) {}
 
+  async canAddWrittenEntry(userId: number): Promise<boolean> {
+    // Găsește abonamentul utilizatorului
+    const subscription = await this.prisma.userSubscription.findFirst({
+      where: { userId },
+      include: { membership: true },
+    });
+  
+    if (!subscription) {
+      throw new Error('Utilizatorul nu are un abonament activ.');
+    }
+  
+    // Numără intrările scrise create azi
+    const todayStart = new Date();
+    todayStart.setHours(0, 0, 0, 0); // Începutul zilei curente
+  
+    const count = await this.prisma.writtenEntry.count({
+      where: {
+        userId,
+        createdAt: {
+          gte: todayStart, // Filtrare pentru intrările create după începutul zilei
+        },
+      },
+    });
+  
+    // Compară cu limita zilnică
+    return count < subscription.membership.nr_maxim_intrari_Scrise;
+  }
+  
+
   async create(userId: number, content: string) {
+    const canAdd = await this.canAddWrittenEntry(userId);
     console.log('Creating WrittenEntry:', { userId, content });
+    if (!canAdd) {
+      throw new Error('S-a atins limita zilnica de intrari scrise pentru acest abonament');
+    }else{
     try{
     return this.prisma.writtenEntry.create({
       data: {
@@ -21,6 +53,7 @@ export class WrittenEntryService {
       console.error('Database error:', error);
       throw new Error('Failed to create WrittenEntry');
     }
+  }
   }
 
   async findAllByUser(userId: number) {
